b7bb8acb568a085150c0403a1eead0db37e54811
dima <87605146+konata12@users.noreply.github.com>
2024-01-27 18:13:07 +0200

    no jwt

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,10 +4,4 @@ const instance = axios.create({
     baseURL: 'http://localhost:5000/api'
 })
 
-instance.interceptors.request.use(config => {
-    config.headers.Authorization = window.localStorage.getItem('token')
-
-    return config
-})
-
 export default instance
